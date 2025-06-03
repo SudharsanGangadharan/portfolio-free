ed406ae5b37f1b053ef41b53f91bce4cdaa373f3
SudharsanGangadharan <sudharsanganga007@gmail.com>
2025-06-03 21:12:28 +0530

    Add files via upload

---
+++ pERSONAL pf/includes/head.php
@@ -0,0 +1,25 @@
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-G8ZWSQNG');</script>
+<!-- End Google Tag Manager -->
+
+    
+<link rel="shortcut icon" type="image/x-icon" href="#">
+<link rel="preload stylesheet" href="assets/css/bootstrap.min.css" as="style" type="text/css" crossorigin="anonymous">
+
+<link rel="preload stylesheet" href="assets/css/style.css" as="style" type="text/css" crossorigin="anonymous">
+<link rel="preload stylesheet" href="assets/css/custom.css" as="style" type="text/css" crossorigin="anonymous">
+<link rel="preload stylesheet" href="assets/css/responsive.css" as="style" type="text/css" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/css/swiper.css" as="style" type="text/css" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/css/aos.css" as="style" type="text/css" crossorigin="anonymous">
+<link rel="stylesheet" href="assets/css/fancybox.css" as="style" type="text/css" crossorigin="anonymous">
+
+
+<link rel="preload stylesheet" href="assets/flaticon/flaticon_normal-icon.css" as="style" type="text/css" crossorigin="anonymous"> 
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
---
+++ pERSONAL pf/includes/script.php
@@ -0,0 +1,8 @@
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.validate.min.js"></script>
+<script src="assets/js/additional-methods.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/swiper.min.js"></script>
+<script src="assets/js/aos.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/fancybox.js"></script>
