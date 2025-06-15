ae7938bccdf5367c88cbf4d3a07b21119344fba4
SudharsanGangadharan <sudharsanganga007@gmail.com>
2025-06-15 13:34:54 +0530

    Delete pERSONAL pf directory

--- pERSONAL pf/includes/script.php
+++
@@ -1,8 +0,0 @@
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/jquery.validate.min.js"></script>
-<script src="assets/js/additional-methods.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/swiper.min.js"></script>
-<script src="assets/js/aos.js"></script>
-<script src="assets/js/main.js"></script>
-<script src="assets/js/fancybox.js"></script>
